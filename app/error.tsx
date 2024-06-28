253c2ca1cea552ec7308f7f4fc9c44ed601ea60a
xugro <82907135+xugro@users.noreply.github.com>
2024-06-28 19:03:28 +0100

    hata sayfasını türkçe yap

--- app/error.tsx
+++ app/error.tsx
@@ -17,14 +17,14 @@ export default function Error({
 
   return (
     <div>
-      <h2>Something went wrong!</h2>
+      <h2>Bir hata oluştu!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
-        Try again
+        Tekrar Dene
       </button>
     </div>
   );
