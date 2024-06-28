ae3a9398e5302085c9967bbf15ad521673e418c6
mkutay <me@mkutay.dev>
2024-06-28 16:35:24 +0300

    nextui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,66 @@
-import type { Metadata } from "next";
-import { Raleway } from "next/font/google";
-import "@/app/globals.css";
-import NavBar from "@/app/ui/navBar";
-import Footer from "@/app/ui/footer";
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@nextui-org/link";
+import clsx from "clsx";
 
-const raleway = Raleway({ subsets: ["latin"] });
+import { Providers } from "@/app/providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
+import { HeartFilledIcon } from "@/components/icons";
 
 export const metadata: Metadata = {
-  title: "TÜBİOME | Türkiye Bilim Olimpiyatları Mezunları Topluluğu",
-  description: "TÜBiome olarak ulusal ve uluslararası olimpiyatlara katılan öğrencilerin kariyerlerinin ilerleyen dönemlerinde yaşıtlarıyla ve olimpiyata hazırlanan alt dönemleriyle iletişim halinde kalmasını amaçlıyoruz.",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${raleway.className} flex flex-col min-h-screen`}>
-        <NavBar/>
-        {children}
-        <Footer/>
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+            <footer className="w-full flex items-center justify-center py-3">
+              <Link
+                isExternal
+                className="flex items-center gap-1 text-current"
+                href="https://www.mkutay.dev/"
+                title="mkutay.dev ana sayfa"
+              >
+                <HeartFilledIcon className="text-primary"/>
+                <p className="text-primary font-semibold">Kutay</p>
+              </Link>
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
---
+++ config/site.ts
@@ -0,0 +1,61 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "TÜBİOME | Türkiye Bilim Olimpiyatları Mezunları Topluluğu",
+  description: "TÜBiome olarak ulusal ve uluslararası olimpiyatlara katılan öğrencilerin kariyerlerinin ilerleyen dönemlerinde yaşıtlarıyla ve olimpiyata hazırlanan alt dönemleriyle iletişim halinde kalmasını amaçlıyoruz.",
+  navItems: [
+    {
+      label: "Branşlar",
+      href: "/branslar",
+    },
+    {
+      label: "Yurtdışı Eğitim",
+      href: "/yurtdisi-egitim",
+    },
+    {
+      label: "Topluluk",
+      href: "/topluluk",
+    },
+    {
+      label: "Etkinlikler",
+      href: "/etkinlikler",
+    },
+    {
+      label: "Biz",
+      href: "/biz",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Ana Sayfa",
+      href: "/",
+    },
+    {
+      label: "Branşlar",
+      href: "/branslar",
+    },
+    {
+      label: "Yurtdışı Eğitim",
+      href: "/yurtdisi-egitim",
+    },
+    {
+      label: "Topluluk",
+      href: "/topluluk",
+    },
+    {
+      label: "Etkinlikler",
+      href: "/etkinlikler",
+    },
+    {
+      label: "Biz",
+      href: "/biz",
+    },
+  ],
+  links: {
+    twitter: "http://www.x.com/tubiome",
+    facebook: "https://www.facebook.com/profile.php?id=61560938087877",
+    youtube: "https://www.youtube.com/@tubiome",
+    instagram: "https://www.instagram.com/turkiyebiome/",
+    linkedin: "http://www.linkedin.com/company/t%C3%BCbiome/",
+  },
+};
