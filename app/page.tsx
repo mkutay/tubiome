3bf7bd87b96ae738bcc04a0c00946d71e2c43d71
mkutay <me@mkutay.dev>
2024-06-29 21:42:56 +0300

    branslar cards on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,27 @@ import { title, subtitle } from "@/components/primitives";
 import { InstagramIcon, YoutubeIcon } from "@/components/icons";
 import BlogPostCards from "@/components/blogPostCards";
 import { Divider } from "@nextui-org/divider";
+import {
+  Card, CardHeader, CardBody, CardFooter
+} from "@nextui-org/card";
+import { Image } from "@nextui-org/image";
 
 export default function Home() {
+  let items: {
+    label: string,
+    href: string,
+    biome: string,
+    color: string,
+    slug: string,
+  }[] = [];
+
+  if (siteConfig.navItems[0].dropdownItems) {
+    items.push(siteConfig.navItems[0].dropdownItems[0]);
+    items.push(siteConfig.navItems[0].dropdownItems[1]);
+    items.push(siteConfig.navItems[0].dropdownItems[3]);
+    items.push(siteConfig.navItems[0].dropdownItems[4]);
+  }
+
   return (
     <section className="flex flex-col items-center justify-center gap-4">
       <div className="inline-block max-w-2xl text-center justify-center">
@@ -46,7 +65,7 @@ export default function Home() {
 
       <Divider className="sm:my-16 my-10"/>
 
-      <div className="max-w-6xl sm:mb-16 mb-10">
+      <div className="max-w-6xl">
         <h1 className={title()}>Son&nbsp;</h1>
         <Link href="/blog/sayfa/1" className={title({ color: "blue" })}>
           <h1 className={title({ color: "blue" })}>
@@ -55,6 +74,37 @@ export default function Home() {
         </Link>
         <div className="gap-3 grid sm:grid-cols-2 grid-cols-1 mt-8 md:mt-10">
           <BlogPostCards startInd={0} endInd={4}/>
+        </div>
+      </div>
+
+      <Divider className="sm:my-16 my-10"/>
+
+      <div className="max-w-6xl sm:mb-16 mb-10">
+        <Link href="/blog/sayfa/1" className={title({ color: "blue" })}>
+          <h1 className={title({ color: "cyan" })}>
+            Olimpiyat&nbsp;
+          </h1>
+        </Link>
+        <h1 className={title()}>Branşları</h1>
+        <div className="gap-3 grid grid-cols-2 mt-8 md:mt-10">
+          {items.map((item, index) => (
+            <Link href={item.href} key={index}>
+              <Card shadow="sm" isPressable isFooterBlurred radius="md">
+                <CardBody className="overflow-visible p-0">
+                  <Image
+                    alt={item.label}
+                    className="object-cove8r"
+                    src={`/fotograflar/branslar/${item.biome}.jpg`}
+                  />
+                </CardBody>
+                <CardFooter
+                  className="before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10"
+                >
+                  <p className="text-white/75 sm:text-xl text-lg font-bold">{item.label}</p>
+                </CardFooter>
+              </Card>
+            </Link>
+          ))}
         </div>
       </div>
     </section>
